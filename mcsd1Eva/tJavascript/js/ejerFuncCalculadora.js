912e380876305bc0978444a31497c3d3acefe680
paulLozano90 <plc90@hotmail.es>
2015-09-29 12:31:05 +0200

    septiembre

---
+++ mcsd1Eva/tJavascript/js/ejerFuncCalculadora.js
@@ -0,0 +1,33 @@
+﻿
+//function calculo(e) {
+
+//    var num1 = document.getElementById("txtNum1").value;
+//    var num2 = document.getElementById("txtNum2").value;
+//    var operando = e.target.value;
+//    var operacion = num1 + operando + num2;
+//    var resultado = eval(operacion);
+
+//    document.getElementById("operando").value = operando;
+//    document.getElementById("resultado").value = resultado;
+//}
+
+//window.onload = function () {
+//    document.getElementById("textNum1").focus();
+//};
+
+var calculo = function(e){
+    
+    var num1 = document.getElementById("txtNum1").value;
+    var num2 = document.getElementById("txtNum2").value;
+    var operando = e.target.value;
+    var operacion = num1 + operando + num2;
+    var resultado = eval(operacion);
+
+    document.getElementById("operando").value = operando;
+    document.getElementById("resultado").value = resultado;
+}
+
+document.getElementById("btnSuma").onclick = calculo;
+document.getElementById("btnResta").onclick = calculo;
+document.getElementById("btnMulti").onclick = calculo;
+document.getElementById("btnDiv").onclick = calculo;
---
+++ mcsd1Eva/tJavascript/js/funciones.js
@@ -0,0 +1,27 @@
+﻿
+var saludo = function (texto) {
+    alert("Hola " + texto.target.outerText);
+}
+
+function saludando(texto) {
+    alert("Hola " + texto.target.outerText);
+}
+
+//El outerText recogera el texto del elemento(boton)
+
+document.getElementById("btn1").onclick = saludo;
+document.getElementById("btn2").onclick = saludando;
+
+//Funcion anonima:
+
+//document.getElementById("btn3").onclick = function (e) {
+//    alert("Hola " + e.target.outerText);
+//}
+
+//Funcion autoinvocable
+
+(function () {
+    document.getElementById("btn4").onclick = function (evt) {
+        alert("Hola " + evt.target.id);
+    };
+})();
