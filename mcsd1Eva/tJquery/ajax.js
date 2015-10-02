93024f2d3b7cd778f3b7b49861b23c6c50d1394b
paulLozano90 <plc90@hotmail.es>
2015-10-02 14:28:05 +0200

    jquery

---
+++ mcsd1Eva/tJquery/ajax.js
@@ -0,0 +1,83 @@
+﻿var url = "https://alumnoscurso.azure-mobile.net/Tables/Alumno/";
+
+$(document).ready(function () {
+
+    $("#lnkGet").click(conGet);
+    $("#lnkGetJson").click(conGetJson);
+    $("#lnkPost").click(conPost);
+    $("#lnkAjax").click(conAjax);
+    $("#lnkLoad").click(conLoad);
+});
+
+function procesarJson(res) {
+
+    var r = "";
+
+    for (var i = 0; i < res.length; i++) {
+
+        r += res[i].nombre + "<br />";
+    }
+
+    $("#resultado").html(r);
+}
+
+function conGet(e) {
+
+    e.preventDefault();
+    $.get(url, procesarJson);
+}
+
+function conGetJson(e) {
+
+    e.preventDefault();
+    $.getJSON(url, procesarJson);
+}
+//Va dar error...
+function conPost(e) {
+
+    e.preventDefault();
+    var o = {
+        nombre: "Luis",
+        apellidos: "yo",
+        edad: 22,
+        nota: 7
+    };
+    $.post(url, JSON.stringify(o), function(res) {
+        console.log(res);
+    });
+}
+
+function conAjax(e) {
+    e.preventDefault();
+    var o = {
+        nombre: "Luis",
+        apellidos: "yo",
+        edad: 22,
+        nota: 7
+    };
+    $.ajax({
+        //Metodo que voy a emplear (post,get...)
+        method: "post",
+        url: url,
+        //Aqui le decimos que hacer despues de acabar
+        success: function(res) {
+            console.log(res);
+        },
+        //Funcion o que hacer si hay error
+        error: function(err) {
+            console.log(err);
+        },
+        data: JSON.stringify(o),
+        //Tipo de datos que voy a enviar
+        datatype: "json",
+        //Nombre de las cabeceras que voy a enviar
+        headers: {"Content-Type":"application/json"
+        }
+    });
+}
+
+function conLoad(e) {
+
+    e.preventDefault();
+    $("#resultado").load(url);
+}
---
+++ mcsd1Eva/tJquery/ejercicioAjax.js
@@ -0,0 +1,85 @@
+﻿var url = "https://alumnoscurso.azure-mobile.net/Tables/Curso/";
+
+$(document).ready(function () {
+
+    crearTabla();
+    $("#nuevoRegistro").hide();
+    $("#btnCrear").click(mostrarRegistro);
+    $("#btnAnadir").click(crearCurso);
+    $("#btnRefrescar").click(crearTabla);
+});
+
+function crearTabla() {
+
+    $.getJSON(url, function (curso) {
+        $("#datosTabla tbody").html("");
+        $.each(curso, function (i, curso) {
+
+            var fila = "<tr>"
+                    + "<td>" + curso.nombre + "</td>"
+                    + "<td>" + curso.duracion + "</td>"
+                    + "<td><button class='btn btn-default btn-curso'>Borrar</button></td>"
+                    + "<td><button class='btn btn-default btn-curso'>Modificar</button></td>"
+                    + "</tr>";
+
+            $(fila).appendTo("#datosTabla tbody");
+        });
+    });
+}
+
+function mostrarRegistro() {
+
+    $("#nuevoRegistro").show();
+}
+
+function datosCurso() {
+
+    var datos = {
+        nombre: $("#txtNombre").val(),
+        duracion: parseInt($("#txtDuracion").val())
+    };
+    return datos;
+}
+
+function crearCurso(e) {
+
+    $("#nuevoRegistro").show();
+    e.preventDefault();
+
+    $.ajax({
+
+        method: "post",
+        url: url,
+        success: function (res) {
+            console.log(res);
+            crearTabla();
+            $("#nuevoRegistro").hide();
+        },
+        error: function (err) {
+            console.log(err);
+        },
+        data: JSON.stringify(datosCurso()),
+        datatype: "json",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+}
+
+function borrarCurso(id) {
+
+    e.preventDefault();
+    
+    $.ajax({
+
+        method: "delete",
+        url: url + "/" + id,
+        success: function (res) {
+            console.log(res);
+            crearTabla();
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
