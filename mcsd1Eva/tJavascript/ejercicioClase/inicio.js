4e2f1b9a8c97da48bca6228d76eabe409f4ed1db
paulLozano90 <plc90@hotmail.es>
2015-10-05 14:27:20 +0200

    ejercicio clase (canvas)

---
+++ mcsd1Eva/tJavascript/ejercicioClase/inicio.js
@@ -0,0 +1,64 @@
+﻿
+var url = "https://alumnoscurso.azure-mobile.net/Tables/Clase19/";
+
+if (!localStorage.getItem("nombre"))
+    location.replace("registro.html");
+
+function borrarSesion() {
+    
+    localStorage.clear();
+    location.replace("registro.html");
+}
+
+function datosSitio() {
+    
+    var datos = {
+        nombre: localStorage.getItem("nombre"),
+        x: parseInt($("#txtX").val()),
+        y: parseInt($("#txtY").val()),
+        ancho: parseInt($("#txtAncho").val()),
+        alto: parseInt($("#txtAlto").val()),
+        color: $("#txtColor").val()
+    };
+    return datos;
+}
+
+function guardarSitio(e) {
+
+    e.preventDefault();
+
+    $.ajax({
+        method: "post",
+        url: url,
+        success: function () {
+            console.log("Datos guardados");
+            mostrarSitio();
+        },
+        error: function (err) {
+            console.log(err);
+        },
+        data: JSON.stringify(datosSitio()),
+        datatype: "json",
+        headers: {"Content-Type": "application/json"}
+    });
+}
+
+function mostrarSitio() {
+
+    var x = parseInt($("#txtX").val());
+    var y = parseInt($("#txtY").val());
+    var ancho = parseInt($("#txtAncho").val());
+    var alto = parseInt($("#txtAlto").val());
+    var color = $("#txtColor").val();
+
+    var canvas = document.getElementById("clase");
+    var contexto = canvas.getContext("2d");
+    
+    contexto.fillStyle = color;
+    //Posicion eje x,y, ancho y alto
+    contexto.fillRect(x, y, ancho, alto);
+}
+
+document.getElementById("usuario").innerHTML = "Hola, "+localStorage.getItem("nombre") + ":";
+document.getElementById("btnBorrar").addEventListener("click", borrarSesion);
+document.getElementById("btnEnviar").addEventListener("click", guardarSitio);
---
+++ mcsd1Eva/tJavascript/ejercicioClase/registro.js
@@ -0,0 +1,18 @@
+﻿
+if (localStorage.getItem("nombre")) {
+    location.replace("inicio.html");
+}
+
+function login() {
+
+    var nombre = document.getElementById("txtNombre").value;
+
+    if (nombre === "") {
+        alert("Nombre vacio");
+        return;
+    }
+    localStorage.setItem("nombre", nombre);
+    location.replace("inicio.html");
+}
+
+document.getElementById("btnEntrar").addEventListener("click", login);
